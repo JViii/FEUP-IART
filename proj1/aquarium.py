02cbca8d53476cf769785c8331812dc4ea3322eb
Diogo Santos <diogofilipe1_santos@hotmail.com>
2021-03-03 13:12:04 +0000

    Adicionei a funcao bfs ao aquarium ja ta o esquema feito

--- proj1/aquarium.py
+++ proj1/aquarium.py
@@ -1,6 +1,7 @@
 from state import State
 from node import Node
 from fill import Fill
+from utils import *
 
 def printAquarium(node):
     aquarium = node.state.aquarium
@@ -11,8 +12,46 @@ def printAquarium(node):
             print(aquarium[i][j], end = " ")
         print("")
 
+def printSequenceOfStates(node):
+    if node.parent == -1:
+        printAquarium(node)
+    else:
+        printSequenceOfStates(node.parent)
 
-aquarium  = [[0, 0, 0, 0, 0, 0],
+def applyOperator(node, notExpanded):
+    # To do: avoid repeated states maybe look to the siblings
+    # To generalize
+    for i in range(5, -1, -1):
+        for j in range(6):
+            newNode = Fill(node, i, j)
+            if newNode != -1: notExpanded.insert(0, newNode)
+
+def bfs(initial_aquarium, rowCap, colCap):
+    currNode = Node(State(copy_list(initial_aquarium), rowCap, colCap))
+    finalNode = -1
+
+    notExpanded = [] # to change to a queue
+
+    while True:
+        if isObjective(currNode):
+            finalNode = currNode
+            break
+
+        # apply operator
+        applyOperator(currNode, notExpanded)
+
+        # selects next node to process
+        if len(notExpanded) == 0:
+            break
+        else:
+            currNode = notExpanded.pop(len(notExpanded) - 1)
+
+    if finalNode == -1:
+        print("There is no solution to this problem")
+    else:
+        printSequenceOfStates(finalNode)
+
+initialAquarium  = [[0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
@@ -22,9 +61,9 @@ aquarium  = [[0, 0, 0, 0, 0, 0],
 rowCap = [1, 5, 2, 5, 3, 1]
 colCap = [4, 3, 3, 2, 2, 3]
 
-initialState = State(aquarium, rowCap, colCap)
-initialNode = Node(initialState)
-newNode = Fill(initialNode, 1, 1).apply()
+# initialState = State(aquarium, rowCap, colCap)
+# initialNode = Node(initialState)
+# newNode = Fill(initialNode, 1, 1).apply()
 
-printAquarium(initialNode)
-printAquarium(newNode)
+# printAquarium(initialNode)
+# printAquarium(newNode)
